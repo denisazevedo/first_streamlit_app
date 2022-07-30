375f4f289d743aedb0ddaa0bd97d8b9e75d7ddc2
Denis C de Azevedo <denis.azevedo@gmail.com>
2022-07-30 17:45:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,6 @@ import streamlit as st
 st.title('My parents New Healthy Diner')
 
 st.header('Breakfast Menu')
-
 st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
