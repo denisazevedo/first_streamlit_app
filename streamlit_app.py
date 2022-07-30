9ae2897a8acb100d227a67cbf5b442e6952824b4
Denis C de Azevedo <denis.azevedo@gmail.com>
2022-07-30 17:52:15 -0300

    add dataframe with panda

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import panda
 
 st.title('''My Mom's New Healthy Diner''')
 
@@ -9,4 +10,8 @@ st.text('🥗 Kale, Spinach & Rocket Smoothie')
 st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
 
+# Build your own
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
