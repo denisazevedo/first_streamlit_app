c78247469dd8aa371685db97cfb0f606293cd094
Denis C de Azevedo <denis.azevedo@gmail.com>
2022-07-30 23:59:30 -0300

    fix st

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,9 @@ st.dataframe(fruits_to_show)
 st.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it as a table
-streamlit.dataframe(fruityvice_normalized)
+st.dataframe(fruityvice_normalized)
