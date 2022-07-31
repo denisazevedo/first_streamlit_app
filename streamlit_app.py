9f57d009158424498449851f424bce0c989d4094
Denis C de Azevedo <denis.azevedo@gmail.com>
2022-07-30 23:58:27 -0300

    add data table from fruityvice data

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,8 @@ st.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
+
+# take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it as a table
+streamlit.dataframe(fruityvice_normalized)
