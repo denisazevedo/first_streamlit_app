deae219a65e7aa38ab31deafa5396b4e2e83ac8a
Denis C de Azevedo <denis.azevedo@gmail.com>
2022-08-01 20:45:13 -0300

    fix missing return

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,11 +44,11 @@ except URLError as e:
   st.error()
     
 # Snowflake
-st.header("The fruit load list contains:")
+st.header("View our fruit List - Add Your Favorites!")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
-    my_cur.fetchall()
+    return my_cur.fetchall()
 
 # Add a button to load the fruit
 if st.button('Get Fruit Load List'):
